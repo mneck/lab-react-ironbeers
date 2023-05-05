e075ed5a15c7e098bf2d2f99ddcdd7a64e9d660f
Matt N <mneckelmann@gmail.com>
2023-05-05 17:21:50 +0200

    Iteration 3 complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,43 @@
 import beersImg from "../assets/beers.png";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function Home() {
+  const [beers, setBeers] = useState(null);
+
+  useEffect(() => {
+    axios.get("https://ih-beers-api2.herokuapp.com/beers").then((response) => {
+      setBeers(response.data);
+    });
+  }, []);
+
+  if (!beers) return <p>Page is loading</p>;
+
+  // const nameOfBeerContributor = (name) => {
+  // return name.toString().split(" ").slice(0, 2).join(" ");
+  // };
+
+  // const contributor = setBeers.contributed_by.toString().split(" ").slice(0, 2).join(" ");
+
   return (
     <div className="divMargin">
       <img src={beersImg} alt="beers" class="imgSpacing" />
-      <h1>All Beers</h1>
-      <p>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-      </p>
+      <h1>All the beers!</h1>
+      {console.log(beers[10])}
+      {beers.map((beer) => (
+        <div className="beerCard">
+          <div className="beerImg">
+            <a href="/beers/:beerId">
+              <img src={beer.image_url} alt={beer.name} className="beerImg" />
+            </a>
+          </div>
+          <div className="beerText">
+            <h3>{beer.name}</h3>
+            <h4>{beer.tagline}</h4>
+            <p>Created by: {beer.contributed_by}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
